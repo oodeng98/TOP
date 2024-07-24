c4140470bd884b95b751334fe4ce5a9d10b08cf8
김재현 <niggaud@naver.com>
2024-07-24 13:49:22 +0900

    feat: 집중시간 별 APP 통계조회 API 구현(TOP3)

--- Backend/top/src/main/java/com/ssafy/top/onedays/presentation/OneDaysController.java
+++ Backend/top/src/main/java/com/ssafy/top/onedays/presentation/OneDaysController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
-@RequestMapping("/dash")
 @RestController
+@RequestMapping("/dash")
+@RequiredArgsConstructor
 public class OneDaysController {
 
     private final OneDaysService oneDaysService;
