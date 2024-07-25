fe487e7edf42cbabf52ee612f7c6d61964526e71
이윤지 <yunji383614@gmail.com>
2024-07-25 10:33:01 +0900

    feat : 친구 신청 API 설계

--- Backend/top/src/main/java/com/ssafy/top/global/exception/ErrorCode.java
+++ Backend/top/src/main/java/com/ssafy/top/global/exception/ErrorCode.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    WHITESPACE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "공백은 사용할 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    FRIEND_SELF_REQUEST(HttpStatus.BAD_REQUEST, "자기 자신에게 친구 신청할 수 없습니다."),
+    FRIEND_ALREADY_REQUESTING(HttpStatus.BAD_REQUEST, "이미 친구 신청이 진행중입니다."),
+    FRIEND_ALREADY_ADDED(HttpStatus.BAD_REQUEST, "이미 친구입니다."),
+    WHITESPACE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "공백은 사용할 수 없습니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
