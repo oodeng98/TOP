9876c6bda8646686f77ec06424c6d8440a0ef7b2
이윤지 <yunji383614@gmail.com>
2024-07-26 14:59:58 +0900

    feat : 친구 삭제 API 설계

--- Backend/top/src/main/java/com/ssafy/top/global/exception/ErrorCode.java
+++ Backend/top/src/main/java/com/ssafy/top/global/exception/ErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     NICKNAME_DUPLICATED(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다."),
     FRIEND_SELF_REQUEST(HttpStatus.BAD_REQUEST, "자기 자신에게 친구 신청할 수 없습니다."),
