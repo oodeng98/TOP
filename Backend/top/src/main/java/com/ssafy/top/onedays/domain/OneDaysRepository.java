13b17f8a44ec9c50e8b88befb375d953a8730d7e
김재현 <niggaud@naver.com>
2024-07-31 14:06:31 +0900

    feat: 목표 시간 수정 API 설계

--- Backend/top/src/main/java/com/ssafy/top/onedays/domain/OneDaysRepository.java
+++ Backend/top/src/main/java/com/ssafy/top/onedays/domain/OneDaysRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.top.onedays.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
