3ecbc0302d1ef2604c4c5e7a92c292439fd00d4c
정태완 <oodeng98@naver.com>
2024-08-02 15:40:55 +0900

    feat: FocusTimeEachPrograms.vue axios 추가

--- Frontend/top/src/components/FocusTimeEachPrograms.vue
+++ Frontend/top/src/components/FocusTimeEachPrograms.vue
@@ -1,83 +1,86 @@
 <template>
   <div class="box">
-    <div clas="element">
+    <div class="element">
+      <div class="text-wrapper-4">프로그램 별 집중 시간</div>
       <div class="overlap-group">
+        <div class="titles">
+          <div class="text-wrapper-3">Programs</div>
+          <div class="text-wrapper-2">사용 시간</div>
+        </div>
         <div class="list">
           <div class="items">
-            <div class="soft-UI-XD-1">
-              <img class="line" alt="Line" src="../static/img/line.png" />
-              <div class="text-wrapper">5:32:03</div>
-              <div class="content">
-                <div class="title">
-                  <div class="div">youtube</div>
-                </div>
-                <img class="icon" alt="icon" src="../static/img/youtube.png" />
+            <div
+              v-for="(app, index) in appList"
+              :key="index"
+              class="soft-UI-XD-1"
+            >
+              <div class="item-content">
+                <img
+                  class="icon"
+                  :src="getImagePath(app.name)"
+                  :alt="app.name"
+                  @error="handleImageError"
+                />
+                <div class="app-name">{{ app.name }}</div>
+                <div class="text-wrapper">{{ formatTime(app.focusTime) }}</div>
               </div>
-            </div>
-            <div class="soft-UI-XD-2">
               <img class="line" alt="Line" src="../static/img/line.png" />
-              <div class="text-wrapper">5:32:03</div>
-              <div class="content">
-                <div class="title">
-                  <div class="div">Chakra Soft UI Version</div>
-                </div>
-                <img class="icon" alt="icon" src="../static/img/youtube.png" />
-              </div>
             </div>
-            <div class="soft-UI-XD-3">
-              <img class="line" alt="Line" src="../static/img/line.png" />
-              <div class="text-wrapper">5:32:03</div>
-              <div class="content">
-                <div class="title">
-                  <div class="div">Chakra Soft UI Version</div>
-                </div>
-                <img class="icon" alt="icon" src="../static/img/youtube.png" />
-              </div>
-            </div>
-            <div class="soft-UI-XD-4">
-              <img class="line" alt="Line" src="../static/img/line.png" />
-              <div class="text-wrapper">5:32:03</div>
-              <div class="content">
-                <div class="title">
-                  <div class="div">Chakra Soft UI Version</div>
-                </div>
-                <img class="icon" alt="icon" src="../static/img/youtube.png" />
-              </div>
-            </div>
-            <div class="soft-UI-XD-5">
-              <img class="line" alt="Line" src="../static/img/line.png" />
-              <div class="text-wrapper">5:32:03</div>
-              <div class="content">
-                <div class="title">
-                  <div class="div">Chakra Soft UI Version</div>
-                </div>
-                <img class="icon" alt="icon" src="../static/img/youtube.png" />
-              </div>
-            </div>
-            <div class="soft-UI-XD-6">
-              <img class="line" alt="Line" src="../static/img/line.png" />
-              <div class="text-wrapper">5:32:03</div>
-              <div class="content">
-                <div class="title">
-                  <div class="div">Chakra Soft UI Version</div>
-                </div>
-                <img class="icon" alt="icon" src="../static/img/youtube.png" />
-              </div>
-            </div>
-          </div>
-          <img class="line" alt="Line" src="../static/img/line.png" />
-          <div class="titles">
-            <div class="text-wrapper-2">사용 시간</div>
-            <div class="text-wrapper-3">Programs</div>
           </div>
         </div>
-        <div class="text-wrapper-4">프로그램 별 집중 시간</div>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      appList: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get(
+          "https://i11a707.p.ssafy.io/api/dash/stats/focus-time"
+        );
+        if (response.status === 200 && response.data.statusCode === "200") {
+          this.appList = response.data.data.appList;
+        } else {
+          console.error("Failed to fetch data:", response.data.message);
+        }
+      } catch (error) {
+        console.error("API request failed:", error);
+      }
+    },
+    formatTime(seconds) {
+      const h = Math.floor(seconds / 3600);
+      const m = Math.floor((seconds % 3600) / 60);
+      const s = seconds % 60;
+      return `${h}:${m < 10 ? "0" : ""}${m}:${s < 10 ? "0" : ""}${s}`;
+    },
+    getImagePath(appName) {
+      try {
+        return require(`../static/img/application_icon/${appName
+          .toLowerCase()
+          .replace(/\s+/g, "")}.png`);
+      } catch (e) {
+        return require("../static/img/application_icon/default.png"); // 기본 이미지 경로
+      }
+    },
+    handleImageError(event) {
+      event.target.src = require("../static/img/application_icon/default.png"); // 기본 이미지 경로
+    },
+  },
+};
+</script>
 
 <style scoped>
 .box {
@@ -100,78 +103,48 @@
   height: 400px;
   position: relative;
   width: 483px;
+  padding: 20px;
 }
 
 .box .list {
-  height: 310px;
-  left: 31px;
-  position: absolute;
-  top: 60px;
-  width: 420px;
+  margin-top: 20px;
 }
 
 .box .items {
-  height: 275px;
-  left: 0;
-  position: absolute;
-  top: 50px;
-  width: 422px;
+  display: flex;
+  flex-direction: column;
 }
 
 .box .soft-UI-XD-1 {
-  height: 34px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 422px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  position: relative;
 }
 
-.box .soft-UI-XD-2 {
-  height: 34px;
-  left: 0;
-  position: absolute;
-  top: 48px;
-  width: 422px;
+.box .item-content {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
 }
 
-.box .soft-UI-XD-3 {
-  height: 34px;
-  left: 0;
-  position: absolute;
-  top: 96px;
-  width: 422px;
+.box .icon {
+  height: 25px;
+  width: 25px;
+  margin-right: 10px;
 }
 
-.box .soft-UI-XD-4 {
-  height: 34px;
-  left: 0;
-  position: absolute;
-  top: 144px;
-  width: 422px;
-}
-
-.box .soft-UI-XD-5 {
-  height: 34px;
-  left: 0;
-  position: absolute;
-  top: 192px;
-  width: 422px;
-}
-
-.box .soft-UI-XD-6 {
-  height: 34px;
-  left: 0;
-  position: absolute;
-  top: 240px;
-  width: 422px;
-}
-
-.box .line {
-  height: 1px;
-  left: 0;
-  position: absolute;
-  top: 39px;
-  width: 421px;
+.box .app-name {
+  color: #2d3748;
+  font-family: "Helvetica-BoldOblique", Helvetica;
+  font-size: 14px;
+  font-weight: 700;
+  letter-spacing: 0;
+  line-height: 19.6px;
+  white-space: nowrap;
+  margin-left: 10px;
+  flex-grow: 1;
+  text-align: left;
 }
 
 .box .text-wrapper {
@@ -179,97 +152,33 @@
   font-family: "Helvetica-BoldOblique", Helvetica;
   font-size: 18px;
   font-weight: 700;
-  left: 340px;
   letter-spacing: 0;
   line-height: 25.2px;
-  position: absolute;
-  top: 0;
   white-space: nowrap;
-  width: 68px;
 }
 
-.box .content {
-  height: 15px;
-  left: 0;
-  position: absolute;
-  top: 3px;
-  width: 195px;
-}
-
-.box .title {
-  height: 15px;
-  left: 20px;
-  position: absolute;
-  top: 0;
-  width: 169px;
-}
-
-.box .div {
-  color: #2d3748;
-  font-family: "Helvetica-BoldOblique", Helvetica;
-  font-size: 14px;
-  font-weight: 700;
-  left: 0;
-  letter-spacing: 0;
-  line-height: 19.6px;
-  position: absolute;
-  top: 0;
-  white-space: nowrap;
-  padding-left: 5px;
-  /* width: 167px; */
-}
-
-.box .icon {
-  height: 25px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 25px;
-}
-
-.box .img {
-  height: 4px;
-  left: 0;
-  position: absolute;
-  top: 21px;
-  width: 421px;
+.box .line {
+  height: 1px;
+  width: 100%;
+  background-color: #e0e0e0;
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 
 .box .titles {
-  height: 12px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 409px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
 }
 
-.box .text-wrapper-2 {
-  color: #a0aec0;
-  font-family: "Helvetica-BoldOblique", Helvetica;
-  font-size: 15px;
-  font-weight: 700;
-  left: 352px;
-  letter-spacing: 0;
-  line-height: 15px;
-  position: absolute;
-  top: 0;
-  left: 340px;
-  white-space: nowrap;
-  width: 52px;
-}
-
+.box .text-wrapper-2,
 .box .text-wrapper-3 {
   color: #a0aec0;
   font-family: "Helvetica-BoldOblique", Helvetica;
   font-size: 15px;
   font-weight: 700;
-  left: 0;
   letter-spacing: 0;
   line-height: 15px;
-  position: absolute;
-  top: 0;
-  white-space: nowrap;
-  width: 54px;
 }
 
 .box .text-wrapper-4 {
@@ -277,12 +186,10 @@
   font-family: "Helvetica-BoldOblique", Helvetica;
   font-size: 18px;
   font-weight: 700;
-  left: 22px;
   letter-spacing: 0;
   line-height: 25.2px;
-  position: absolute;
-  top: 11px;
-  white-space: nowrap;
-  width: 165px;
+  margin-bottom: 10px;
+  text-align: left;
+  margin-left: 20px;
 }
 </style>
