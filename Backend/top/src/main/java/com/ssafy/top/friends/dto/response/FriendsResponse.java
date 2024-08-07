a20fe916c30758280d5371384efd4fe84464b780
이윤지 <yunji383614@gmail.com>
2024-08-07 13:33:38 +0900

    feat : Friends API에 Swagger 적용

--- Backend/top/src/main/java/com/ssafy/top/friends/dto/response/FriendsResponse.java
+++ Backend/top/src/main/java/com/ssafy/top/friends/dto/response/FriendsResponse.java
@@ -1,16 +1,21 @@
 package com.ssafy.top.friends.dto.response;
 
 import com.ssafy.top.users.domain.Users;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
+@Schema(description = "친구 목록 조회 응답 DTO")
 @Data
 @Builder
 public class FriendsResponse {
+    @Schema(description = "친구 아이디")
     private Long userId;
 
+    @Schema(description = "친구 닉네임")
     private String nickname;
 
+    @Schema(description = "친구 화면 공유 여부")
     private Boolean screenShare;
 
     public static FriendsResponse toDto(Users user) {
