91b89686d8b1d7b9ecee7ce38de9e3ba0fccb685
연상헌 <kevinyeonkr@naver.com>
2024-08-07 15:10:04 +0900

    asd

--- extension/host/install_host.bat
+++
@@ -1,2 +0,0 @@
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.google.chrome.example.echo" /ve /t REG_SZ /d "%~dp0com.google.chrome.example.echo-win.json" /f
-pip install psutil
--- extension/host/uninstall_host.bat
+++
@@ -1,2 +0,0 @@
-REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.google.chrome.example.echo" /f
-REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\com.google.chrome.example.echo" /f
