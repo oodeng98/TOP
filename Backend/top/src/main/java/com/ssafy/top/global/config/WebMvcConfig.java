00345f241661e8ca9eda9b5368a98b345f61cde2
ktk3939 <rkdxorud3939@naver.com>
2024-08-09 10:56:33 +0900

    refactor: CORS Configuration 수정

--- Backend/top/src/main/java/com/ssafy/top/global/config/WebMvcConfig.java
+++ Backend/top/src/main/java/com/ssafy/top/global/config/WebMvcConfig.java
@@ -7,16 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final long MAX_AGE_SECS = 3600;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 // GET, POST, PATCH, DELETE, OPTIONS 메서드 허용
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
+                .allowedHeaders("*");
     }
 }
