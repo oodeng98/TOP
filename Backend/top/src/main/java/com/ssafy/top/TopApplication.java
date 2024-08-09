b9218da9bf34917d08d04d1d408596d5c3b5acd9
이윤지 <yunji383614@gmail.com>
2024-08-09 09:05:07 +0900

    feat : SecurityConfig 활성화

--- Backend/top/src/main/java/com/ssafy/top/TopApplication.java
+++ Backend/top/src/main/java/com/ssafy/top/TopApplication.java
@@ -1,17 +1,13 @@
 package com.ssafy.top;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.TimeZone;
-
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableScheduling
 public class TopApplication {
 	
