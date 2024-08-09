9af9ad84430bcbf1388d3358861ff0341ca5dddc
ktk3939 <rkdxorud3939@naver.com>
2024-08-09 10:54:10 +0900

    refactor: CORS Configuration 수정

--- Backend/top/src/main/java/com/ssafy/top/TopApplication.java
+++ Backend/top/src/main/java/com/ssafy/top/TopApplication.java
@@ -2,10 +2,7 @@ package com.ssafy.top;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,19 +10,5 @@ public class TopApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TopApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-						.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedHeaders("*")
-						.allowedMethods("*");
-			}
-		};
 	}
 }
