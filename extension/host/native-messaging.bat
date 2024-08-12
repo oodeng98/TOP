3bdfa8431113e52326453fc44e00ea59b8f2e2cb
연상헌 <kevinyeonkr@naver.com>
2024-08-12 17:48:19 +0900

    delete extension

--- extension/host/native-messaging.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-python "%~dp0/native-messaging.py" %*
--- extension/host/uninstall_host.bat
+++
@@ -1,2 +0,0 @@
-REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.google.chrome.top" /f
-REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\com.google.chrome.top" /f
