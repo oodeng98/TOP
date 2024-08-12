d99c34a7207faccb6a3d4cb2b944cdfa19f2e22e
정태완 <oodeng98@naver.com>
2024-08-12 10:26:35 +0900

    feat: 로그인 기능 추가

--- Frontend/top/src/App.vue
+++ Frontend/top/src/App.vue
@@ -5,12 +5,25 @@
 </template>
 
 <script>
-import Dashboard from "./components/Dashboard.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    Dashboard,
+  // created()는 Vue 인스턴스가 생성된 직후 호출됩니다.
+  created() {
+    this.login();
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.get(
+          "https://i11a707.p.ssafy.io/api/user/login"
+        );
+        console.log("로그인 요청 성공:", response.data);
+      } catch (error) {
+        console.error("로그인 요청 실패:", error);
+      }
+    },
   },
 };
 </script>
