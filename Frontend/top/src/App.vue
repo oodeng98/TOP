121b31c2a75f7735fde5ac62aeae2efdf10c7ef7
정태완 <oodeng98@naver.com>
2024-08-12 14:12:31 +0900

    fix: 로그인 해결 시도

--- Frontend/top/src/App.vue
+++ Frontend/top/src/App.vue
@@ -5,28 +5,17 @@
 </template>
 
 <script>
-import axios from "axios";
-import Dashboard from "./components/Dashboard.vue"; // Dashboard 컴포넌트 import
+import Dashboard from "./components/Dashboard.vue";
 
 export default {
   name: "App",
   components: {
-    Dashboard, // Dashboard 컴포넌트 등록
+    Dashboard,
   },
-  created() {
-    this.login();
-  },
-  methods: {
-    async login() {
-      try {
-        const response = await axios.get(
-          "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=1055451669456-7j0mkark9qc3r3stsjt82jbt3hl3d248.apps.googleusercontent.com&scope=profile%20email&state=zCNq0aJOX1TTG4RP7lykKplQGxRteR_xVvZIgJ1kfgs%3D&redirect_uri=https://i11a707.p.ssafy.io/api/login/oauth2/code/google"
-        );
-        console.log("로그인 요청 성공:", response.data);
-      } catch (error) {
-        console.error("로그인 요청 실패:", error);
-      }
-    },
+  mounted() {
+    const googleAuthUrl = "https://i11a707.p.ssafy.io/api/user/login";
+
+    window.location.href = googleAuthUrl;
   },
 };
 </script>
