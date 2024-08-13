f89cb04671998c05decd43610d27a910db244474
정태완 <oodeng98@naver.com>
2024-08-13 15:02:04 +0900

    design: 반응형 페이지 제거 시도

--- Frontend/top/src/main.js
+++ Frontend/top/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 import { BootstrapVue3 } from "bootstrap-vue-3";
-import { createPinia } from 'pinia';
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 
@@ -12,5 +12,3 @@ app.use(pinia);
 
 app.use(BootstrapVue3);
 app.mount("#app");
-
-
