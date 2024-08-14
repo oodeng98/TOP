e37063b3cd935aa1d96ed0442df7cc7e136f223b
ktk3939 <rkdxorud3939@naver.com>
2024-08-14 09:38:39 +0900

    refactor: Long -> long으로 변경

--- Backend/top/src/main/java/com/ssafy/top/appfocustimes/dto/response/FocusTimeListDayResponse.java
+++ Backend/top/src/main/java/com/ssafy/top/appfocustimes/dto/response/FocusTimeListDayResponse.java
@@ -13,8 +13,8 @@ import lombok.NoArgsConstructor;
 public class FocusTimeListDayResponse {
 
     @Schema(description = "단위 시간")
-    private Long startTime;
+    private long startTime;
 
     @Schema(description = "집중 시간")
-    private Long focusTime;
+    private long focusTime;
 }
