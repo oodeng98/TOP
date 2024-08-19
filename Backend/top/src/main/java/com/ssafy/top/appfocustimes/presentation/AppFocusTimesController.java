9a98fba93a0bb05c3fdafff705b1f7d7fb4f6341
김재현 <niggaud@naver.com>
2024-08-19 16:16:41 +0900

    fix: 시간 로직 수정

--- Backend/top/src/main/java/com/ssafy/top/appfocustimes/presentation/AppFocusTimesController.java
+++ Backend/top/src/main/java/com/ssafy/top/appfocustimes/presentation/AppFocusTimesController.java
@@ -163,9 +163,9 @@ public class AppFocusTimesController {
     @GetMapping("/dash/stats/focus-time/percent")
     public ResponseEntity<?> findFocusTimePercent(HttpSession session){
 
-        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
+//        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
 
-        CommonResponseDto<?> response = appFocusTimesService.findFocusTimePercentByEmail(sessionUser.getEmail());
+        CommonResponseDto<?> response = appFocusTimesService.findFocusTimePercentByEmail("Timo1@gmail.com");
 
         return ResponseEntity.ok().body(response);
     }
