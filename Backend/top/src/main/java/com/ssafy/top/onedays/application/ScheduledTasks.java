585fed0295f9a36c11bbb9819f347d7f90fb3b32
KimJ4ehyun <kimjaehyun158@gmail.com>
2024-08-19 19:29:56 +0900

    fix

--- Backend/top/src/main/java/com/ssafy/top/onedays/application/ScheduledTasks.java
+++ Backend/top/src/main/java/com/ssafy/top/onedays/application/ScheduledTasks.java
@@ -4,12 +4,9 @@ import com.ssafy.top.appfocustimes.application.AppFocusTimesService;
 import com.ssafy.top.onedays.domain.OneDaysRepository;
 import com.ssafy.top.users.domain.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.time.LocalDate;
-import java.time.ZoneId;
 
 @Component
 @RequiredArgsConstructor
@@ -21,17 +18,14 @@ public class ScheduledTasks {
 
     private final UsersRepository usersRepository;
 
-    @Scheduled(cron = "0 26 8 * * *", zone = "UTC")
+    @Scheduled(cron = "0 32 10 * * *", zone = "UTC")
     public void updateAllUsersOneDayFocusTime() {
 //        LocalDate yesterday = LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
         LocalDate yesterday = LocalDate.of(2024, 8, 13);
         usersRepository.findAll().forEach(user -> {
             int totalFocusTime = appFocusTimesService.findTodayTotalFocusTimeForScheduleByUserIdAndDateData(user.getId(), yesterday);
             if (totalFocusTime != 0) {
-                oneDaysRepository.findByUserIdAndDateData(user.getId(), yesterday).ifPresent(oneDays -> {
-                    oneDays.updateFocusTime(totalFocusTime);
-
-                });
+                oneDaysRepository.findByUserIdAndDateData(user.getId(), yesterday).ifPresent(oneDays -> oneDays.updateFocusTime(totalFocusTime));
             }
         });
     }
